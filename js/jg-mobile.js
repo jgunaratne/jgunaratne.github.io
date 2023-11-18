1721e2450e2761f721c257e018c1efa5706948a0
Junius Gunaratne <jgunaratne@gmail.com>
2023-11-18 11:12:10 -0800

    Allow mobile previews to appear

--- js/jg-mobile.js
+++ js/jg-mobile.js
@@ -6,42 +6,42 @@ class JGMobile extends HTMLElement {
 
   connectedCallback() {
     this.shadowRoot.innerHTML = `
-      <style>
-        .mobile {
-          width: 520px;
-          height: 1040px;
-          position: relative;
-          cursor: zoom-in;
-        }
+      <style>       
+        @media only screen and (min-width: 1024px) {
+          .mobile {
+            width: 520px;
+            height: 1040px;
+            position: relative;
+            cursor: zoom-in;
+          }
 
-        .mobile-content {
-          position: absolute;
-          top: 55px;
-          left: 58px;
-          border-radius: 50px;
-          background-color: white;
-        }
-        
-        :host(.no-frame) .mobile-content {
-          border-radius: 50px;
-          border: 1px solid var(--hairline);
-          padding: 80px 20px;
-          background-color: rgb(35, 33, 35);
-        }
-        
-        .mobile-frame {
-          position: absolute;
-          width: 520px;
-          height: 1040px;
-          background-image: url(../img/iphone.png);
-          background-size: contain;
-          background-repeat: no-repeat;
-          top: 0;
-          left: 0;
-        }
+          .mobile-content {
+            position: absolute;
+            top: 55px;
+            left: 58px;
+          }
+          
+          :host(.no-frame) .mobile-content {
+            border-radius: 50px;
+            border: 1px solid var(--hairline);
+            padding: 80px 20px;
+            background-color: rgb(35, 33, 35);
+          }
+          
+          .mobile-frame {
+            position: absolute;
+            width: 520px;
+            height: 1040px;
+            background-image: url(../img/iphone.png);
+            background-size: contain;
+            background-repeat: no-repeat;
+            top: 0;
+            left: 0;
+          }
 
-        :host(.no-frame) .mobile-frame {
-          display: none;
+          :host(.no-frame) .mobile-frame {
+            display: none;
+          }
         }
       </style>
       <div class="mobile">
