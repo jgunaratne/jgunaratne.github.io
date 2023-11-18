fd3117602ca4931ee4c369aeac4b303ad89a4bc7
Junius Gunaratne <junius@google.com>
2023-11-17 20:23:41 -0800

    Create lightbox

--- js/home.js
+++ js/home.js
@@ -2,3 +2,17 @@ document.querySelector('.menu-control').addEventListener('click', function () {
   document.querySelector('jg-nav').classList.toggle('open');
   document.querySelector('.container').classList.toggle('menu-open');
 });
+
+function toggleLightbox(thumbnail) {
+  let lightbox = document.querySelector('jg-lightbox');
+  if (thumbnail) {
+    lightbox.thumbnail = thumbnail.children[0];
+  }
+  lightbox.classList.toggle('open');
+}
+
+document.querySelectorAll('.thumbnail').forEach(function (thumbnail) {
+  thumbnail.addEventListener('click', function () {
+    toggleLightbox(thumbnail);
+  });
+});
